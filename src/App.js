b76efe97bb36a215c2e3210fabbfdc68fa72ec9f
b3kaRSTMV <bekaskg2002@gmail.com>
2022-10-06 00:39:29 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import Header from './components/header/Header';
-import {Routes,Route} from 'react-router-dom';
-
+import { Routes, Route } from 'react-router-dom';
+import SearchSection from './page/SearchSection/SearchSection';
 
 
 function App() {
   return (
     <div className='App'>
-      <Header/>
+      <Header />
 
       <Routes>
-        
+        <Route path='/SearchSection' element={<SearchSection />} >
+        </Route>
       </Routes>
     </div>
   );
